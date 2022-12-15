32023ee556f42fbfb4f4261d5bdcb4b5a7957942
Vanessa Sirigu <vane.sirigu@gmail.com>
2022-12-15 10:52:11 +0100

    add Text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+  size: number
+  element: string
+  weight: string
+}
+
+export const Text = ({ size, element, weight }: Props) => {
+  return <div>Text</div>
+}
